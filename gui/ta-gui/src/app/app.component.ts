d836ebd82d2e5d161df0696f4366135f0d8b7594
LuanaPorciuncula <lpb@cin.ufpe.br>
2019-09-29 15:43:33 -0300

    botao e funcionalidade do formulario para cadastro de alunos

--- gui/ta-gui/src/app/app.component.ts
+++ gui/ta-gui/src/app/app.component.ts
@@ -10,23 +10,11 @@ import { AlunoService } from './aluno.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   constructor(private alunoService: AlunoService) {}
-
-   aluno: Aluno = new Aluno();
-   alunos: Aluno[] = [];
-   cpfduplicado: boolean = false;
-
-   criarAluno(a: Aluno): void {
-     if (this.alunoService.criar(a)) {
-       this.alunos.push(a);
-       this.aluno = new Aluno();
-     } else {
-       this.cpfduplicado = true;
-     }
-   }
-
-   onMove(): void {
-      this.cpfduplicado = false;
-   }
-
+   aluno: Aluno = {nome: "", cpf: "", email: "", loginGitHub:""};
+   alunoService = new AlunoService();
+   
+   gravar(a: Aluno): void {
+    this.alunoService.gravar(a);
+    this.aluno = {nome: "", cpf: "", email: "",  loginGitHub:""};
+ }
 }
